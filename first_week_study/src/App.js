bd8568844522ac59c8d48bfc03d9d022a08b2464
wongakim-99 <202021000@sangmyung.kr>
2024-03-20 23:48:02 +0900

    Counter 컴포넌트, Say컴포넌트 추가
    Counter 컴포넌트에서는 constructor 메서드를 선언하지 않고도 state 초깃값을 설정했습니다.
    this.setState를 사용하여 객체 대신 함수 인자를 전달하였고 this.setState가 끝난 후 특정 작업 실행을 위해 setState의 두번째 파라미터로 콜백 함수를 등록하여 작업을 처리했습니다.
    App 컴포넌트에서 Counter컴포넌트를 렌더링하고 개발자 도구를 탭하여 열어보면 "방금 setState가 호출되었습니다." 라는 문구가 잘 표시될 것입니다.
    
    ------------------------------------------
    
    Say컴포넌트에서는 useState를 사용하였습니다.

--- first_week_study/src/App.js
+++ first_week_study/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/**Counter컴포넌트 렌더링 */
+// import Counter from "./Counter";
+
+// const App = () => {
+//   return <Counter />;
+// };
+
+// export default App;
+
+/**Say 컴포넌트 렌더링 */
+import Say from "./Say";
+
+const App = () => {
+  return <Say />;
+};
 
 export default App;
---
+++ first_week_study/src/Say.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const Say = () => {
+  const [message, setMessage] = useState("");
+  const onClickEnter = () => setMessage("Hello World!");
+  const onClickLeave = () => setMessage("Goodbye World!");
+  const [color, setColor] = useState("black");
+
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: "red" }} onClick={() => setColor("red")}>
+        빨간색
+      </button>
+      <button style={{ color: "green" }} onClick={() => setColor("green")}>
+        초록색
+      </button>
+      <button style={{ color: "blue" }} onClick={() => setColor("blue")}>
+        파란색
+      </button>
+      <button style={{ color: "black" }} onClick={() => setColor("black")}>
+        검정색
+      </button>
+    </div>
+  );
+};
+
+export default Say;
