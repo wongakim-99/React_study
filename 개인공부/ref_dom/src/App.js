a2dceef95a7f929830e7f7abef19ad5d34abe76e
wongakim-99 <202021000@sangmyung.kr>
2024-03-23 22:50:46 +0900

    ref:DOM에 이름 달기
    리엑트를 다루는 기술 5장 ref:DOM에 이름 달기 챕터를 참고로 공부했습니다.
    
    파일 추가사항은 ValidationSample.js, ValidationSample.css 이렇게 두 가지가 있고
    App.js 같은 경우는 위의 ValidationSample.js 파일에 맞춰서 렌더링을 해줬습니다.

---
+++ 개인공부/ref_dom/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ValidationSample from "./ValidationSample";
+
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
+
+export default App;
