1e29f97bd0bc85f9003935019375a2c04c456349
wongakim-99 <202021000@sangmyung.kr>
2024-04-07 17:05:15 +0900

    useMemo 복습
    useMemo를 사용하여 함수 컴포넌트 내부에서 발생하는 연산을 최적화함.
    리스트에 숫자를 추가하여 추가된 숫자들의 평균을 보여주는 간단한 예제

--- 개인공부/hooks/src/AverageVo1.js
+++ 개인공부/hooks/src/AverageVo1.js
@@ -2,14 +2,24 @@ import React, { useState } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 중...");
-  if (numbers.length === 0) return 0;
+  if (numbers.length === 0) return 0; //입려된 배열numbers가 비어있는지 확인 -> 만약 비어있다면
+  //즉, 배열에 요소가 없다면 평균값을 구할 수 없으므로 0을 반환
   const sum = numbers.reduce((a, b) => a + b);
   return sum / numbers.length;
 };
-
+/**getAverage함수는 입력된 숫자 배열의 평균값을 계산하는 함수로, 배열이 비어있는 경우에는0을 반환
+ * 그렇지 않은 경우에는 배열의 요소들을 합산하여 평균값을 계산하여 반환
+ */
 const AverageVol1 = () => {
   const [list, setList] = useState([]);
+  /**이 코드는 React의 useState 훅을 사용하여 컴포넌트의 상태를 관리한다. 여기서 list는 입력된
+   * 숫자들을 담는 배열이고 setList는 list의 값을 업데이트 하는 함수이다.
+   * 초기값으로 빈 배열을 전달하여 list를 초기화한다.
+   */
   const [number, setNumber] = useState("");
+  /**setNumber은 number은 현재 입력된 숫자를 나타내는 상태이고
+   * setNumber은 number을 업데이트 하는 함수 -> 초기값으로 빈 문자열을 전달하여 number을 초기화 한다.
+   */
 
   const onChange = (e) => {
     setNumber(e.target.value);
@@ -20,6 +30,10 @@ const AverageVol1 = () => {
     setList(nextList);
     setNumber("");
   };
+  /**'등록' 버튼이 클릭될 때 호출되는 이벤트 핸들러. 이 함수는 list 배열에 현재 입력된 숫자를 추가하고
+   * setList를 호출하여 list 상태를 업데이트한다. 추가된 후에는 입력 필드를 초기화하기 위해
+   * setNumber를 호출하여 number 상태를 빈 문자열로 업데이트함
+   */
 
   return (
     <div>
--- 개인공부/hooks/src/AverageVol2.js
+++ 개인공부/hooks/src/AverageVol2.js
@@ -28,6 +28,15 @@ const AverageVol2 = () => {
     setNumber("");
   };
   const avg = useMemo(() => getAverage(list), [list]);
+  /**getAverage의 반환값을 avg 변수에 저장하는 데, 이때 'list'배열의 변화에 따라 변경됨
+   * 여기서 [list]는 useMemo에 전달된 두 번째 매개변수인 의존성 배열임
+   * 이 배열은 해당 값이 변경될 때에만 메모이제이션된 값이 다시 계산되도록 함
+   * 즉, list 배열이 변경될 때마다 getAverage(list) 함수를 호출하여 평균값을 계산하고,
+   * 그 값을 avg 변수에 저장함. 그리고 렌더링 시에는 list 배열이 변경되지 않으면 이전에 계산된 평균값 반환
+   *
+   * 이렇게 함으로써 'list' 배열이 변경되지 않는 한 평균값을 다시 계산하지 않고 이전에
+   * 계산된 값을 재사용하여 성능을 최적화
+   */
 
   return (
     <div>
