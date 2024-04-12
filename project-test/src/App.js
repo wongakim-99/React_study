bd47fc21a2cad45b6e895e2559a4d2e83def7bbb
wongakim-99 <202021000@sangmyung.kr>
2024-04-13 03:36:29 +0900

    처음부터 재조정
    to do list 파일 새롭게 만드는 중입니다.

--- project-test/src/App.js
+++ project-test/src/App.js
@@ -1,44 +1,73 @@
 import "./App.css";
-import List from "./List";
 import { useState } from "react";
-import { v4 as uuid } from "uuid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-const initialTodos = [
-  // {
-  //   id: uuid(),
-  //   todo: "리액트 공부하기",
-  //   completed: false,
-  // },
-  // {
-  //   id: uuid(),
-  //   todo: "블로그 쓰기",
-  //   completed: false,
-  // },
-];
+let todoItemId = 0;
+
+const TodoItemInputField = (props) => {
+  const [input, setInput] = useState("");
+
+  const onSubmit = () => {
+    //버튼 눌렀을 때 onSubmit callback 콜 해주기
+    props.onSubmit(input);
+    setInput("");
+  };
+
+  return (
+    <div>
+      <TextField
+        id="todo-item-input"
+        label="Todo Item"
+        variant="outlined"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <Button variant="outlined" onClick={onSubmit}>
+        Submit
+      </Button>
+    </div>
+  );
+};
+
+const TodoItem = (props) => {
+  return (
+    <li>
+      <span>{props.todoItem.todoItemContent}</span>
+    </li>
+  );
+};
+
+const TodoItemList = (props) => {
+  const todoList = props.todoItemList.map((todoItem, index) => {
+    return <TodoItem key={index} todoItem={todoItem} />;
+  });
+  return (
+    <div>
+      <ul>{todoList}</ul>
+      {/**TodoItemList 콤포넌트 props 로 등록된 Todo 아이템들 받기*/}
+    </div>
+  );
+};
 
 function App() {
-  const [todos, setTodo] = useState(initialTodos);
+  const [todoItemList, setTodoItemList] = useState([]);
 
-  const handleSubmit = (newTodo) => {
-    const todo = {
-      id: uuid(),
-      todo: newTodo,
-      completed: false,
-    };
-    setTodo((prevState) => [...prevState, todo]);
+  const onSubmit = (newTodoItem) => {
+    setTodoItemList([
+      ...todoItemList,
+      {
+        id: todoItemId++,
+        todoItemContent: newTodoItem,
+        isFinished: false,
+      },
+    ]);
   };
-  const deleteTodo = (id) => {
-    setTodo((prevState) => prevState.filter((todo) => todo.id !== id));
-  };
+
   return (
     <div className="App">
-      <h1 id="heading">to-do-list</h1>
-      <List
-        todos={todos}
-        handleSubmit={handleSubmit}
-        handleClickTodo={setTodo}
-        deleteTodo={deleteTodo}
-      />
+      <TodoItemInputField onSubmit={onSubmit} />
+      <TodoItemList todoItemList={todoItemList} />
     </div>
   );
 }
