56df364b897b4887e8c371f952034767736d62f3
wongakim-99 <202021000@sangmyung.kr>
2024-04-13 16:21:22 +0900

    firebase 설치
    백엔드 연동시도

--- project-test/src/App.js
+++ project-test/src/App.js
@@ -3,7 +3,35 @@ import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
-let todoItemId = 0;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  setDoc,
+  doc,
+} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDx3dyblA0kIM9UKHbnP6fdnB_Y55FE7rY",
+  authDomain: "todo-list-web-app-ceed0.firebaseapp.com",
+  projectId: "todo-list-web-app-ceed0",
+  storageBucket: "todo-list-web-app-ceed0.appspot.com",
+  messagingSenderId: "536573983103",
+  appId: "1:536573983103:web:d3d5be2469de067eee424a",
+  measurementId: "G-Y4E5XY42SQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
@@ -31,16 +59,34 @@ const TodoItemInputField = (props) => {
 };
 
 const TodoItem = (props) => {
+  const style = props.todoItem.isFinished
+    ? { textDecoration: "line-through" }
+    : {};
   return (
     <li>
-      <span>{props.todoItem.todoItemContent}</span>
+      <span style={style} onClick={() => props.onTodoItemClick(props.todoItem)}>
+        {props.todoItem.todoItemContent}
+      </span>
+      <Button
+        variant="outlined"
+        onClick={() => props.onRemoveClick(props.todoItem)}
+      >
+        Remove
+      </Button>
     </li>
   );
 };
 
 const TodoItemList = (props) => {
   const todoList = props.todoItemList.map((todoItem, index) => {
-    return <TodoItem key={index} todoItem={todoItem} />;
+    return (
+      <TodoItem
+        key={index}
+        todoItem={todoItem}
+        onTodoItemClick={props.onTodoItemClick}
+        onRemoveClick={props.onRemoveClick}
+      />
+    );
   });
   return (
     <div>
@@ -53,21 +99,59 @@ const TodoItemList = (props) => {
 function App() {
   const [todoItemList, setTodoItemList] = useState([]);
 
-  const onSubmit = (newTodoItem) => {
+  const onSubmit = async (newTodoItem) => {
+    const docRef = await addDoc(collection(db, "todoItem"), {
+      todoItemContent: newTodoItem,
+      isFinished: false,
+    });
     setTodoItemList([
       ...todoItemList,
       {
-        id: todoItemId++,
+        id: docRef.id,
         todoItemContent: newTodoItem,
         isFinished: false,
       },
     ]);
   };
 
+  const onTodoItemClick = async (clickedTodoItem) => {
+    const todoItemRef = doc(db, "todoItem", clickedTodoItem.id);
+    await setDoc(
+      todoItemRef,
+      { isFinished: !clickedTodoItem.isFinished },
+      { merge: true }
+    );
+    setTodoItemList(
+      todoItemList.map((todoItem) => {
+        if (clickedTodoItem.id === todoItem.id) {
+          return {
+            id: clickedTodoItem.id,
+            todoItemContent: clickedTodoItem.todoItemContent,
+            isFinished: !clickedTodoItem.isFinished,
+          };
+        } else {
+          return todoItem;
+        }
+      })
+    );
+  };
+
+  const onRemoveClick = (removedTodoItem) => {
+    setTodoItemList(
+      todoItemList.filter((todoItem) => {
+        return todoItem.id !== removedTodoItem.id;
+      })
+    );
+  };
+
   return (
     <div className="App">
       <TodoItemInputField onSubmit={onSubmit} />
-      <TodoItemList todoItemList={todoItemList} />
+      <TodoItemList
+        todoItemList={todoItemList}
+        onTodoItemClick={onTodoItemClick}
+        onRemoveClick={onRemoveClick}
+      />
     </div>
   );
 }
