b075506bbcb7c511b7f77ecae08986fe5a9fbcdd
wongakim-99 <202021000@sangmyung.kr>
2024-08-29 16:44:02 +0900

    #6 - 리덕스 미들웨어를 통한 비동기 작업관리(5)
    오픈 소스 커뮤니티에 이미 올라와 있는 redux-logger 미들웨어를 설치하고 사용.
    
    명령어를 사용하여 redux-logger를 설치

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -5,9 +5,11 @@ import App from "./App";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
-import loggerMiddleware from "./lib/loggerMiddleware";
+//import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
